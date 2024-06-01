c5639874a2ffa11a68241dc147db0b489956b564
luminousbinary <boolwrld@outlook.com>
2024-06-01 06:59:39 +0100

    feat : added html

--- Dashboard/HealthCare/fetchUsersData.js
+++ Dashboard/HealthCare/fetchUsersData.js
@@ -1,21 +1,24 @@
 // The username and password
 // DO NOT store credentials in your JS file like this
 // asume the data is being called from a serer
-import TOKEN  from "./config.js";
-let url= 'https://fedskillstest.coalitiontechnologies.workers.dev' ;
+import TOKEN from "./config.js";
+let url = "https://fedskillstest.coalitiontechnologies.workers.dev";
 
 // Authenticate (dummy API)
 fetch(url, {
-	headers: {
-		'Authorization': `Basic ${TOKEN}`
-	}
-}).then(function (response) {
-	if (response.ok) {
-		return response.json();
-	}
-	throw response;
-}).then(function (data) {
-	console.log(data);
-}).catch(function (error) {
-	console.warn(error);
-});
+  headers: {
+    Authorization: `Basic ${TOKEN}`,
+  },
+})
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw response;
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.warn(error);
+  });
